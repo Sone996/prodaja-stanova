5ae23887e7f958c8c14962de93add70223be84ea
nebojsa.ilic <nebojsa.ilic@factoryww.com>
2021-10-29 08:40:59 +0200

    installing packages

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				white: '#ffffff',
+				green: "#DFF2BF",  //bg-green
+				darkGreen: "#4F8A10", //text green
+				red: "#FFD2D2",  //bg-red
+				darkRed: "#DC2626",
+				darkRed: "#D8000C", //text red
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
